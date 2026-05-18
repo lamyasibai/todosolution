18a87d4b2463667f72c1c1350471654f3aba58a4
lamyaa2004 <sibailamya@gmail.com>
2026-05-18 21:33:30 +0300

    Initial commit

---
+++ ToDoSolution/Web/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using NTierTodoApp.Business;
+
+namespace NTierTodoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly TaskService taskService;
+
+        public HomeController(TaskService service)
+        {
+            taskService = service;
+        }
+
+        /// <summary>
+        /// يعرض قائمة المهام.
+        /// </summary>
+        public IActionResult Index()
+        {
+            var tasks = taskService.GetTasks();
+            return View(tasks);
+        }
+
+        /// <summary>
+        /// إضافة مهمة جديدة.
+        /// </summary>
+        [HttpPost]
+        public IActionResult AddTask(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+                taskService.AddTask(title);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// تعيين مهمة كمكتملة.
+        /// </summary>
+        [HttpPost]
+        public IActionResult CompleteTask(int id)
+        {
+            taskService.CompleteTask(id);
+            return RedirectToAction("Index");
+        }
+
+     
+        
+        
+    }
+}
